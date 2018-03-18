be783013edfd5a0b3f2d0e330c7999bc44c43ba3
Jamie Scaife <JamieOnUbuntu@users.noreply.github.com>
2018-03-18 23:44:32 +0000

    Create index.php

---
+++ blog/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!--Copyright Jamie Scaife-->
+<!--Legal Information at https://www.jamieweb.net/contact-->
+
+<head>
+    <title>Blog</title>
+    <meta name="description" content="Jamie Scaife's Blog">
+    <meta name="keywords" content="Jamie, Scaife, jamie scaife, jamiescaife, jamieonubuntu, jamie90437, jamie90437x, jamieweb, jamieweb.net">
+    <meta name="author" content="Jamie Scaife">
+    <link href="/jamie.css" rel="stylesheet">
+    <link href="https://www.jamieweb.net/blog/" rel="canonical">
+</head>
+
+<body>
+
+<?php include "navbar.php" ?>
+
+<div class="body">
+    <h1>Blog</h1>
+    <hr>
+<?php //include "bloglist.php";
+bloglist("blog"); ?>
+</div>
+
+<?php include "footer.php" ?>
+
+</body>
+
+</html>
