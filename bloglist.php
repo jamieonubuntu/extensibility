8d82e618c0913e9128b597c5a0714f399f28e5f0
Jamie Scaife <JamieOnUbuntu@users.noreply.github.com>
2018-03-18 22:48:25 +0000

    Fully working version except for year display

--- bloglist.php
+++ bloglist.php
@@ -21,16 +21,24 @@
     <h1>Blog</h1>
     <hr>
 <?php $bloglist = json_decode(file_get_contents('blog/object.json', true));
-//echo $bloglist->blog->{2018}->{"disabling-tls1.0-tls1.1"}->title;
-
-
 foreach($bloglist->blog as $year) {
-    //echo var_dump($year);
+    echo "\n    <br><div class=\"blog-group\">
+        <div class=\"blog-year\"><h1>2018</h1></div>
+        <div class=\"blog-brace1\"></div>
+        <div class=\"blog-brace2\"></div>
+        <div>
+            <div class=\"blog-brace3\"></div>
+            <div class=\"blog-brace4\"></div>
+            <div class=\"blog-brace5\"></div>
+        </div>
+        <div class=\"blog-list\">\n";
     foreach($year as $post) {
-        echo "<h3 class=\"no-mar-bottom\"><a href=\"/blog/" . $post->uri . "/\">" . $post->title . "</a></h3>
-        <p class=\"two-no-mar\"><b>" . $post->longdesc . "</b></p>
-        <p class=\"two-mar-top\">" . $post->date . "</p>";
+        echo "            <h3 class=\"no-mar-bottom\"><a href=\"/blog/" . $post->uri . "/\">" . $post->title . "</a></h3>
+            <p class=\"two-no-mar\"><b>" . $post->longdesc . "</b></p>
+            <p class=\"two-mar-top\">" . $post->date . "</p>\n\n";
     }
+    echo "        </div>
+    </div>";
 }
 
 ?>
