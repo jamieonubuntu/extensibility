cf83db4311b0e9eca96aca2f3a994128b32b2c72
Jamie Scaife <JamieOnUbuntu@users.noreply.github.com>
2018-03-25 02:12:39 +0100

    Added support for homepage 'recents' list, temp solution to year display
    Getting the property name in JSON is a bit difficult, I have a solution but this is acceptable as a temporary measure

--- bloglist.php
+++ bloglist.php
@@ -9,10 +9,28 @@
         }
     }
 
+    if($location === "recents") {
+    $recentsCount = 0;
+        foreach($bloglist->blog as $year) {
+            foreach($year as $post) {
+                $recentsCount++;
+                echo "\n            <a href=\"/blog/" . $post->uri . "/\">
+                <h4 class=\"no-mar-bottom\">" . $post->title . "</h4>
+                <h5 class=\"two-no-mar\">" . $post->shortdesc . "</h5>
+                <h5 class=\"two-mar-top\">" . $post->date . "</h5>
+            </a>";
+                if($recentsCount >= 4) {
+                    break 2;
+                }
+            }
+        }
+    }
+
     if($location === "blog") {
+        $latestYear = 2018; //Temporary year code
         foreach($bloglist->blog as $year) {
             echo "\n    <br><div class=\"blog-group\">
-        <div class=\"blog-year\"><h1>2018</h1></div>
+        <div class=\"blog-year\"><h1>" . $latestYear-- . "</h1></div>
         <div class=\"blog-brace1\"></div>
         <div class=\"blog-brace2\"></div>
         <div>
