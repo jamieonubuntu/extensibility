efb0712ef0ded2a9c6403527f21a70c04ec9c74f
Jamie Scaife <JamieOnUbuntu@users.noreply.github.com>
2018-03-30 22:58:39 +0100

    Full category page is now generated automatically

--- bloglist.php
+++ bloglist.php
@@ -49,13 +49,30 @@
                 echo "            </p>\n";
             }
             echo "        </div>
-    </div>";
+    </div>\n";
         }
     }
     elseif($location === "tag") {
-        echo "\n    <h1>Category: '" . $category . "'</h1>
-    <hr>
+        echo "<!DOCTYPE html>
+<html lang=\"en\">
 
+<!--Copyright Jamie Scaife-->
+<!--Legal Information at https://www.jamieweb.net/contact-->
+
+<head>
+    <title>Blog</title>
+    <meta name=\"description\" content=\"Blog posts in category: '" . $category . "'\">
+    <meta name=\"keywords\" content=\"Jamie, Scaife, jamie scaife, jamiescaife, jamieonubuntu, jamie90437, jamie90437x, jamieweb, jamieweb.net\">
+    <meta name=\"author\" content=\"Jamie Scaife\">
+    <link href=\"/jamie.css\" rel=\"stylesheet\">
+    <link href=\"https://www.jamieweb.net/blog/category/" . strtolower($category) . "/\" rel=\"canonical\">
+</head>
+
+<body>\n\n";
+        include "navbar.php";
+        echo "\n<div class=\"body\">
+    <h1>Category: '" . $category . "'</h1>
+    <hr>
     <div class=\"blog-list\">\n";
         foreach($bloglist->blog as $year) {
             foreach($year as $post) {
@@ -66,13 +83,17 @@
         <p class=\"two-no-mar\">" . $post->date . "</p>
         <p class=\"tags\">\n";
                     foreach($tags as $tag) {
-                        echo "            <b><a href=\"/blog/category/" . strtolower($tag) . "/\"><b><span class=\"tag-" . strtolower($tag) . "\">" . $tag . "</span></b></a>\n";
+                        echo "            <b><a href=\"/blog/category/" . strtolower($tag) . "/\"><span class=\"tag-" . strtolower($tag) . "\">" . $tag . "</span></a></b>\n";
                     }
                     echo "        </p>\n";
                 }
             }
         }
         echo "    </div>\n";
+        include_once "footer.php";
+        echo "\n\n</body>
+
+</html>";
     }
 }
 ?>
